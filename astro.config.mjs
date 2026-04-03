034cc096ad0acc917ea0fadac5c1ad6f92110e58
yanbin.86 <yanbin.86@jd.com>
2026-04-03 15:05:25 +0800

    新增构建配置

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,9 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+	// 站点主域名
+	site: 'https://misaka789.github.io',
+	// 资源根路径
+	base : '/blog',
+	//integrations: [mdx(), sitemap()],
 });
