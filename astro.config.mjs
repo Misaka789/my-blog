4c251de418bd3b5452c0eefd7539f8534261af94
Misaka789 <3192422357@qq.com>
2026-04-04 23:06:34 +0800

    Refine blog post experience on style

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,17 @@
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
+import rehypeMermaid from 'rehype-mermaid';
 
 export default defineConfig({
 	site: 'https://misaka789.github.io',
 	base: '/my-blog/',
 	integrations: [mdx(), sitemap()],
+	markdown: {
+		syntaxHighlight: {
+			type: 'shiki',
+			excludeLangs: ['mermaid'],
+		},
+		rehypePlugins: [[rehypeMermaid, { strategy: 'pre-mermaid' }]],
+	},
 });
